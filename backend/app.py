c29369b4d54ade52f39e8f007fbdc69b1c32b60a
Jiaju Chen <malcolmcjj13@gmail.com>
2025-01-09 00:09:39 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -140,4 +140,4 @@ def chat_history():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)

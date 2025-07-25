f0c1713eb85c2f4cadf24652812062f9105d3e7b
Jiaju Chen <malcolmcjj13@gmail.com>
2025-07-25 10:07:31 -0500

    updated page switching

--- frontend/src/components/bookRead/ReadPage.js
+++ frontend/src/components/bookRead/ReadPage.js
@@ -436,7 +436,7 @@ const ReadChatPage = () => {
         console.log('moving to next page', currentPageRef.current);
         if (isKnowledge && !isAskedRef.current) {
             console.log('isKnowledge and isAsked', isKnowledge, isAskedRef.current);
-            if (currentPageRef.current != 7 && currentPageRef.current != 10 && currentPageRef.current != 12) {
+            if (currentPageRef.current != 7 && currentPageRef.current != 10 && currentPageRef.current != 12 && currentPageRef.current != 14) {
                 return;
             }
         }

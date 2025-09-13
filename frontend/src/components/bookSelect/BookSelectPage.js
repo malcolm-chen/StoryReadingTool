d3d0610cf51264fdde9f17f5deeff8afe5ca201e
Jiaju Chen <malcolmcjj13@gmail.com>
2025-09-13 15:17:12 -0500

    updated UI

--- frontend/src/components/bookSelect/BookSelectPage.js
+++ frontend/src/components/bookSelect/BookSelectPage.js
@@ -41,14 +41,14 @@ const BookSelectPage = () => {
     const isFavorite = (bookTitle) => favorites.includes(bookTitle);
 
     const handleOpenBook = (bookTitle) => {
-        navigate('/greet', { state: {
-            title: bookTitle,
-            user: user
-        } });
-        // navigate('/read', { state: {
+        // navigate('/greet', { state: {
         //     title: bookTitle,
         //     user: user
-        // } });   
+        // } });
+        navigate('/read', { state: {
+            title: bookTitle,
+            user: user
+        } });   
     };
 
     console.log(favorites);

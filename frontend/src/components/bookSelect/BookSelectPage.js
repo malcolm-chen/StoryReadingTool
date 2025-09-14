e3931e2c05d0dfff9fc3e2ef5c7c6aaebfb2db90
Jiaju Chen <malcolmcjj13@gmail.com>
2025-09-14 11:33:37 -0500

    added chat back

--- frontend/src/components/bookSelect/BookSelectPage.js
+++ frontend/src/components/bookSelect/BookSelectPage.js
@@ -41,14 +41,11 @@ const BookSelectPage = () => {
     const isFavorite = (bookTitle) => favorites.includes(bookTitle);
 
     const handleOpenBook = (bookTitle) => {
-        // navigate('/greet', { state: {
+        a
+        // navigate('/read', { state: {
         //     title: bookTitle,
         //     user: user
-        // } });
-        navigate('/read', { state: {
-            title: bookTitle,
-            user: user
-        } });   
+        // } });   
     };
 
     console.log(favorites);
